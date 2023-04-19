dd9d5bcd79f89a3a6bef7baf0e552280700327de
RaySlash <stevemathewjoy@gmail.com>
2023-04-19 19:17:26 +1000

    add source button

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const bigButton = document.getElementById('bigbutton');
 bigButton.addEventListener('click', changeBackgroundColor);
 
+const sourceButton = document.getElementById('source');
+sourceButton.addEventListener('click', goToGithub);
+
 //Random muted color generator
 var randomColor = getRandomColor();
 function getRandomColor() {
-    var intensity = 100;
+    const intensity = 100;
     const red = Math.floor(Math.random() * intensity);
     const green = Math.floor(Math.random() * intensity);
     const blue = Math.floor(Math.random() * intensity);
     const alpha = 1;
     return `rgba(${red},${green},${blue},${alpha})`;
 }
-    
+
 // Change background
 function changeBackgroundColor() {
     const root = document.documentElement;
@@ -19,4 +22,9 @@ function changeBackgroundColor() {
     randomColor = getRandomColor(); //update the var on every button-press
     const text = document.getElementById('text');
     text.textContent = randomColor;
+}
+
+// Link to git
+function goToGithub() {
+    location.href = 'https://github.com/RaySlash/color-gen';
 }
